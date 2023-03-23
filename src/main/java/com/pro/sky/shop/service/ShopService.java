1b48bf3d0c63ea78056951d8ce2bc3fcc762c768
Anastasia <sychkova.nastyusha@mail.ru>
2023-03-23 10:44:00 +0400

    hw 2.12 done

--- src/main/java/com/pro/sky/shop/service/ShopService.java
+++ src/main/java/com/pro/sky/shop/service/ShopService.java
@@ -14,18 +14,18 @@ import java.util.stream.Collectors;
 
 public class ShopService {
 
-    private static List<Shop> shop = new ArrayList<>();
+    private static List<Shop> shops = new ArrayList<>();
 
     public List<Shop> add(List<Integer> ids) {
         List<Shop> newShop = ids.stream()
                 .map(id -> new Shop(id))
                 .collect(Collectors.toList());
 
-        shop.addAll(newShop);
+        shops.addAll(newShop);
         return newShop;
     }
 
     public List<Shop> get() {
-        return shop;
+        return shops;
     }
 }
